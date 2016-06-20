ad92c14c8b9c4d87d356726094b84c282d1c306e
John R. Ray <jray@redhat-vdi.usfornax.ifornax.ray.com>
2016-06-20 11:32:22 -0600

    Updated dotfiles, updated ssh config

--- chef/knife.rb
+++ chef/knife.rb
@@ -4,7 +4,7 @@ current_dir = File.dirname(__FILE__)
 log_level                :info
 log_location             STDOUT
 node_name                'USERNAME'
-client_key               "#{current_dir}/jray_chef.pem"
+client_key               "#{current_dir}/USERNAME_chef.pem"
 validation_client_name   'gps-ocx-ifdc-validator'
 validation_key           "#{current_dir}/gps-ocx-ifdc-validator.pem"
 chef_server_url          'https://chef-server/organizations/gps-ocx-ifdc'
